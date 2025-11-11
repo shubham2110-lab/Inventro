007c35b51cf957a0aac88b1d6217cec3951a0607
Libritor <51269250+Libritor@users.noreply.github.com>
2025-11-11 14:32:44 -0500

    Change database settings to use PostgreSQL

--- inventro/settings.py
+++ inventro/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +44,8 @@ INSTALLED_APPS = [
     'users',
     'dashboard',
     'rest_framework',
+    'inventory',
+
 ]
 
 MIDDLEWARE = [
@@ -78,12 +81,18 @@ WSGI_APPLICATION = 'inventro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB", "inventro"),
+        "USER": os.environ.get("POSTGRES_USER", "inventro"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", ""),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
+
 
 
 # Password validation
