c920005fc2cb6eb740cfbe6dc874521b268c047e
Libritor <51269250+Libritor@users.noreply.github.com>
2025-11-16 01:13:33 -0500

    Add ready method to ProductsConfig
    Added a ready method to import signal handlers.

--- products/apps.py
+++ products/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class ProductsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'products'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "products"
+
+    def ready(self):
+        # Import signal handlers
+        from . import signals  # noqa: F401
