b6cac6199e0313d52a9057e4b4684aca596d1326
Libritor <51269250+Libritor@users.noreply.github.com>
2025-11-18 15:33:33 -0500

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test, login_required
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import AddUserForm
+
+def _is_admin(user):
+    return user.is_superuser
+
+@user_passes_test(_is_admin)
+def add_user(request):
+    if request.method == "POST":
+        form = AddUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            role = form.cleaned_data.get("role")
+
+            # Map roles to Django flags
+            if role == "ADMIN":
+                user.is_superuser = True
+                user.is_staff = True
+            elif role == "MANAGER":
+                user.is_superuser = False
+                user.is_staff = True
+            else:
+                user.is_superuser = False
+                user.is_staff = False
+
+            user.save()
+            messages.success(request, f"User '{user.username}' created.")
+            return redirect("dashboard_home")
+    else:
+        form = AddUserForm()
+    return render(request, "users/add_user.html", {"form": form})
